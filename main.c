5bd608622b3e7706d9499e38824d29e3abce1beb
sunraku <82903974+sunraku@users.noreply.github.com>
2022-11-11 00:42:14 -0500

    added PID v1.0, merged madgwick.c --> main.c

--- main.c
+++ main.c
@@ -1,44 +1,38 @@
-#include <stdio.h>
+S DF#include <stdio.h>
 #include "madgwickFilter.h"
-#include "madgwick.c"
 #include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BMP280.h> 
-#include <SPIFlash.h>
 #include <Servo.h>
 #include "I2Cdev.h"
+#include <BMP388_DEV.h>
 #include <Adafruit_ISM330DHCX.h>
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+//Useful functions
+//tone(output pin, tone, duration)
+//servo.write(theta). theta is degrees. make sure also to add delay(500) or something after
+//digitalWrite(ledpin,(LOW or HIGH)). low is off. high is on.
 
 //global variables
-float ax_g,ay_g,az_g,gx_rad,gy_rad,gz_rad,pressure,altitude,temperature;
-int pflight;
-int R_LED = 38;
-int G_LED = 34;
-int B_LED = 35;
-int Buzzer = 28;
-int Pyro1 = 22;
-int Pyro2 = 2;
-int Pyro3 = 29;
-int Pyro4 = 37;
-int TVCXpin = 9;
-int TVCYpin = 15;
-Servo TVCXservo;
-Servo TVCYservo;
-Adafruit_BMP280 bmp;
+float ax_g, ay_g,az_g,gx_rad,gy_rad,gz_rad,pressure,altitude,temperature;
+float roll=0.0, pitch=0.0, yaw=0.0;
+struct quaternion q_est = { 1, 0, 0, 0};       // initialize with as unit vector with real component  = 1
+int ax, ay, az;
+int gx, gy, gz;
+BMP388_DEV bmp;
 Adafruit_ISM330DHCX imu;
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-#define OUTPUT_READABLE_IMU
 
-#define CHIPSIZE MB64
-SPIFlash flash(1);
-uint8_t pageBuffer[256];
-String serialCommand;
-char printBuffer[128];
-uint16_t page;
-uint8_t offset, dataByte;
-uint16_t dataInt;
-String inputString, outputString;
+int TVCXpin = 33;
+int TVCYpin = 29;
+int Parachutepin = 0;
+Servo TVCX;
+Servo TVCY;
+Servo Parachute;
+
+int pflight = 0;
+int R_LED = 37;
+int G_LED = 13;
+int B_LED = 14;
+int Buzzer = 36;
 
 void setup() {
   // setup code
@@ -46,19 +40,11 @@ void setup() {
   pinMode(R_LED, OUTPUT);
   pinMode(G_LED, OUTPUT);
   pinMode(B_LED, OUTPUT);
-  pinMode(Pyro1, OUTPUT);
-  pinMode(Pyro2, OUTPUT);
-  pinMode(Pyro3, OUTPUT);
-  pinMode(Pyro4, OUTPUT);
-  if(!bmp.begin()){
-    //barometer configuration
-    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-  }
-  //IMU configuration
+  //barometer
+  bmp388.begin(SLEEP_MODE, OVERSAMPLING_X16, OVERSAMPLING_X2, IIR_FILTER_4, TIME_STANDBY_5MS);
+  bmp388.startNormalConversion();
+  //imu
+  ism330dhcx.begin_I2C();
   imu.initialize();
   imu.setAccelRange(LSM6DS_ACCEL_RANGE_4_G); //set accel range (in gs)
   imu.setGyroRange(LSM6DS_GYRO_RANGE_500_DPS); //set gryo range (in degrees/s)
@@ -67,9 +53,12 @@ void setup() {
   imu.configInt1(false, false, true); // accelerometer DRDY on INT1 (dont know what these lines do)
   imu.configInt2(false, true, false); // gyro DRDY on INT2
   //TVC servos
-  TVCXservo.attach(TVCXpin);
-  TVCYservo.attach(TVCYpin);
+  TVCX.attach(TVCXpin);
+  TVCY.attach(TVCYpin);
+  Parachute.attach(Parachutepin);
+  
 }
+
 void loop() {
   // main code, to run repeatedly:
   update_sensors();
@@ -81,27 +70,128 @@ void loop() {
   }
 }
 
-void tvc(){
-    
+float kp;
+float kd;
+float ki;
+double setpoint[3] = {0,0,0};
+double divisor;
+double errSum[3];
+double lastErr[3];
+double output[3]; 
+float correction_angle[3];
+int sampletime = 0.025; //in seconds (25ms)
 
+void tvc(){
+  float error[3] = {roll,pitch,yaw};
+  errSum += error * sampletime;
+  double dErr[3] = (error - lastErr)/sampletime;
+  output = (kp*error + ki*errSum + kd*dErr)/divisor;
+  error = lastErr;
+  correction_angle = output;
+  servo_actuation(correction_angle);
 }
+
+void servo_actuation(correction_angle){
+  TVCX.write();
+  TVCY.write();
+  //more stuff
+}
+
 void update_sensors(void){
   sensors_event_t accel;
   sensors_event_t gyro;
-  sensors_event_t temp;
-  imu.getEvent(&accel, &gyro, &temp);
   //update sensor values
+  bmp.getMeasurements(temperature,pressure,altitude);    //in C,hPa,m
+  imu.getEvent(&accel, &gyro); 
   gx_rad = gyro.gyro.x;
   gy_rad = gyro.gyro.y;   //in rad/s
   gz_rad = gyro.gyro.z;
   ax_g = accel.acceleration.x / 9.81;
   ay_g = accel.acceleration.y / 9.81;   //in gs, or is m/s^2 fine?
   az_g = accel.acceleration.z / 9.81; 
-  temperature = temp.temperature;
-  pressure = bmp.readPressure();    //in Pa
-  altitude = bmp.readAltitude(1013.25); //needs to be adjusted to local forecast. in meters
   //madgwick filter 
-  float roll=0.0, pitch=0.0, yaw=0.0;
   madgwick_update(ax_g,ay_g,az_g,gx_rad,gy_rad,gz_rad);
   eulerAngles(q_est,&roll,&pitch,&yaw);
+}
+
+struct quaternion quat_mult (struct quaternion L, struct quaternion R){
+    
+    
+    struct quaternion product;
+    product.q1 = (L.q1 * R.q1) - (L.q2 * R.q2) - (L.q3 * R.q3) - (L.q4 * R.q4);
+    product.q2 = (L.q1 * R.q2) + (L.q2 * R.q1) + (L.q3 * R.q4) - (L.q4 * R.q3);
+    product.q3 = (L.q1 * R.q3) - (L.q2 * R.q4) + (L.q3 * R.q1) + (L.q4 * R.q2);
+    product.q4 = (L.q1 * R.q4) + (L.q2 * R.q3) - (L.q3 * R.q2) + (L.q4 * R.q1);
+    
+    return product;
+}
+
+// Updates q_est. gyro angular velocity components are in rad/s, accelerometer components are normalized
+void madgwick_update(float ax, float ay, float az, float gx, float gy, float gz){
+    //Variables and constants
+    struct quaternion q_est_prev = q_est;
+    struct quaternion q_est_dot = {0};           
+    const struct quaternion q_g_ref = {0, 0, 0, 1};
+    struct quaternion q_a = {0, ax, ay, az};    // raw acceleration values (not normalized)
+    float F_g [3] = {0};                        // objective function for gravity
+    float J_g [3][4] = {0};                     // jacobian matrix
+    struct quaternion gradient = {0};           // gradient 
+    
+    // Attitude estimate from gyro with numerical integration 
+    struct quaternion q_w;                   // places gyroscope readings in a quaternion
+    q_w.q1 = 0;                              // real component 0
+    q_w.q2 = gx;
+    q_w.q3 = gy;
+    q_w.q4 = gz;
+    quat_scalar(&q_w, 0.5);                  
+    q_w = quat_mult(q_est_prev, q_w);        // dq/dt = (1/2)q*w
+
+    //Attitude estimate from acc with gradient descent 
+    quat_Normalization(&q_a);              // normalize acc quaternion
+    F_g[0] = 2*(q_est_prev.q2 * q_est_prev.q4 - q_est_prev.q1 * q_est_prev.q3) - q_a.q2;    //Compute objective function for gravity
+    F_g[1] = 2*(q_est_prev.q1 * q_est_prev.q2 + q_est_prev.q3* q_est_prev.q4) - q_a.q3;
+    F_g[2] = 2*(0.5 - q_est_prev.q2 * q_est_prev.q2 - q_est_prev.q3 * q_est_prev.q3) - q_a.q4;
+    
+    //Compute jacobian matrix
+    J_g[0][0] = -2 * q_est_prev.q3;
+    J_g[0][1] = 2 * q_est_prev.q4;
+    J_g[0][2] = -2 * q_est_prev.q1;
+    J_g[0][3] = 2 * q_est_prev.q2;
+    J_g[1][0] = 2 * q_est_prev.q2;
+    J_g[1][1] = 2 * q_est_prev.q1;
+    J_g[1][2] = 2 * q_est_prev.q4;
+    J_g[1][3] = 2 * q_est_prev.q3;
+    J_g[2][0] = 0;
+    J_g[2][1] = -4 * q_est_prev.q2;
+    J_g[2][2] = -4 * q_est_prev.q3;
+    J_g[2][3] = 0;
+    
+    // compute gradient = J_g'*F_g
+    gradient.q1 = J_g[0][0] * F_g[0] + J_g[1][0] * F_g[1] + J_g[2][0] * F_g[2];
+    gradient.q2 = J_g[0][1] * F_g[0] + J_g[1][1] * F_g[1] + J_g[2][1] * F_g[2];
+    gradient.q3 = J_g[0][2] * F_g[0] + J_g[1][2] * F_g[1] + J_g[2][2] * F_g[2];
+    gradient.q4 = J_g[0][3] * F_g[0] + J_g[1][3] * F_g[1] + J_g[2][3] * F_g[2];
+    
+    // gradient normalization
+    quat_Normalization(&gradient);
+  
+    //sensor fusion
+    quat_scalar(&gradient, BETA);             // multiply normalized gradient by beta
+    quat_sub(&q_est_dot, q_w, gradient);        // subtract above from q_w, the integrated gyro quaternion
+    quat_scalar(&q_est_dot, DELTA_T);
+    quat_add(&q_est, q_est_prev, q_est_dot);     // Integrate orientation rate to find position
+    quat_Normalization(&q_est);                 // normalize the orientation of the estimate
+}
+
+//returns as pointers, roll pitch and yaw from q, using right hand system (Roll,x,phi)(Pitch,y,theta)(Yaw,z,psi) 
+void eulerAngles(struct quaternion q, float* roll, float* pitch, float* yaw){
+    
+    *yaw = atan2f((2*q.q2*q.q3 - 2*q.q1*q.q4), (2*q.q1*q.q1 + 2*q.q2*q.q2 -1));  
+    *pitch = -asinf(2*q.q2*q.q4 + 2*q.q1*q.q3);                                  
+    *roll  = atan2f((2*q.q3*q.q4 - 2*q.q1*q.q2), (2*q.q1*q.q1 + 2*q.q4*q.q4 -1));
+    
+    *yaw *= (180.0f / PI);
+    *pitch *= (180.0f / PI);
+    *roll *= (180.0f / PI);
+
 }
